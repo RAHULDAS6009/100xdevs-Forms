469b0bb9a3da3e9e7b36c2940fbcc0960e6a470b
RAHULDAS6009 <das206053@gmail.com>
2025-06-10 09:55:58 +0530

    delete frontend folder

--- apps/frontend/app/[formid]/edit/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Editor } from "@/components/DynamicEditor";
-
-export default async function Home({ params }: { params: { formid: string } }) {
-  const formId = (await params).formid;
-  return <Editor formId={formId} />;
-}
