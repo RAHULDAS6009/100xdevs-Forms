057304264ca8597a9157368fe2dd16619761a1f1
RAHULDAS6009 <das206053@gmail.com>
2025-06-23 07:28:33 +0530

    deploy-bug-1

--- apps/web/components/Logo.tsx
+++ apps/web/components/Logo.tsx
@@ -1,45 +1,36 @@
 "use client";
+
 import { useState } from "react";
 import { DM_Serif_Display } from "next/font/google";
 
-export const dmSerifDisplayItalic = DM_Serif_Display({
+const dmSerifDisplayItalic = DM_Serif_Display({
   weight: "400",
   style: "italic",
   subsets: ["latin"],
   variable: "--dm-serif-display",
 });
 
-export const dmSerifDisplay = DM_Serif_Display({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--dm-serif-display",
-});
-
 export default function Logo() {
-  const [hovered, sethovered] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
   return (
     <button
-      onMouseEnter={() => sethovered(!hovered)}
-      onMouseLeave={() => sethovered(!hovered)}
-      className={`flex group transform transition-all ease-in-out duration-500 opacity-70 text-gray-800 hover:ml-0.5  hover:opacity-100 ${dmSerifDisplayItalic.className}  hover:text-[1.55rem]  cursor-pointer text-black text-2xl   `}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className={`flex group transition-all duration-500 ease-in-out text-2xl cursor-pointer text-black opacity-70 hover:opacity-100 hover:ml-0.5 hover:text-[1.55rem] ${dmSerifDisplayItalic.className}`}
     >
-      100x <AnimatedText animationType={"one"} />
-      {/* </span> */}
+      100x <AnimatedText animationType="one" />
     </button>
   );
 }
 
 const words = " Forms*".split("");
-// @ts-ignore
-function AnimatedText({ animationType }) {
-  const [resetKey, setResetKey] = useState(0);
 
-  const handleRepeat = () => {
-    setResetKey((prev) => prev + 1);
-  };
+function AnimatedText({ animationType }: { animationType: string }) {
+  const [resetKey] = useState(0);
 
   return (
-    <div className="w-full text-center font-black uppercase  border-b-2 border-gray-300 outline-none">
+    <div className="w-full text-center font-black uppercase border-b-2 border-gray-300 outline-none">
       <div key={resetKey} className={`animate ${animationType}`}>
         {words.map((char, i) => (
           <span key={i} style={{ animationDelay: `${i * 0.09}s` }}>
