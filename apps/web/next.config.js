9db4a242e9e9be30d6a26599914e8fca32a0c52d
RAHULDAS6009 <das206053@gmail.com>
2025-06-23 07:44:14 +0530

    deploy-bug-2

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  allowedDevOrigins: ["http://ec2-44-210-130-181.compute-1.amazonaws.com"],
 };
 
 export default nextConfig;
