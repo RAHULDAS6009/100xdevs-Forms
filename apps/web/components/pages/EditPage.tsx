2291dc96bc8de729397430ee0a7fde96ddf4c5f2
RAHULDAS6009 <das206053@gmail.com>
2025-06-29 09:39:31 +0530

    deploy-bug-fixed

--- apps/web/components/pages/EditPage.tsx
+++ apps/web/components/pages/EditPage.tsx
@@ -8,7 +8,7 @@ import { useAppDispatch, useAppSelector } from "../../lib/hooks";
 import { updateForm } from "../../lib/slices/FormSlice";
 import axios from "axios";
 import { redirect, usePathname } from "next/navigation";
-export const BACKEND_URL = "http://localhost:5005";
+export const BACKEND_URL = "http://localhost:5000";
 
 export default function EditPage({ formid }: { formid: string }) {
   const router = usePathname();
